4d5b5cf3d8c6a37daa04bf2a9e47e0546b27aea2
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-06-15 23:01:17 +0900

    Rename: scss 경로 수정

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import header from "@/app/components/styles/Header.module.scss";
+import header from "@/app/styles/Header.module.scss";
 import { useMenuStore } from "@/store";
 import { IoMenuOutline, IoCloseOutline } from "react-icons/io5";
 
