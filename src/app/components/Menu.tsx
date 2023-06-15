35b05838b04b2827c8fb11a7f2e6a1193027c6ce
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-06-15 21:48:36 +0900

    Feat: Menu Router 설정

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -1,15 +1,30 @@
+"use client";
 import React from "react";
 import menu from "@/app/components/styles/Menu.module.scss";
+import { useMenuStore } from "@/store";
+import Link from "next/link";
 
 const Menu = () => {
+  const onMenu = useMenuStore((state) => state.onMenu);
+  const handleMenu = useMenuStore((state) => state.handleMenu);
   return (
-    <nav className={menu.wrapper}>
-      <div>
-        <p>Profile</p>
-        <p>Introduce</p>
-        <p>Project</p>
-      </div>
-    </nav>
+    <>
+      {onMenu && (
+        <nav className={menu.wrapper}>
+          <div className={menu.container}>
+            <Link href="/profile" onClick={handleMenu}>
+              <p>Profile</p>
+            </Link>
+            <Link href="/introduce" onClick={handleMenu}>
+              <p>Introduce</p>
+            </Link>
+            <Link href="/project" onClick={handleMenu}>
+              <p>Project</p>
+            </Link>
+          </div>
+        </nav>
+      )}
+    </>
   );
 };
 
