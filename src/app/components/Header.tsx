dbd73910fd7ebc50e85c7e3ce5f8534dad325a1c
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-06-15 18:05:21 +0900

    Feat: Menu 버튼 구현

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,14 @@
+"use client";
 import React from "react";
 import header from "@/app/components/styles/Header.module.scss";
+import { IoMenuOutline } from "react-icons/io5";
 
 const Header = () => {
-  return <header></header>;
+  return (
+    <header className={header.wrapper}>
+      <IoMenuOutline cursor={"pointer"} className={header.menu__button} />
+    </header>
+  );
 };
 
 export default Header;
