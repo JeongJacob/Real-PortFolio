d83f3b62ef4b6262f8907cd08c41fae66aa6cb98
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-06-15 17:49:04 +0900

    Design: Intro 초기 레이아웃 구현

---
+++ src/app/components/Intro.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import intro from "@/app/components/styles/Intro.module.scss";
+
+const Intro = () => {
+  return (
+    <section>
+      <div className={intro.background}>
+        <div className={intro.text__container}>
+          <p className={intro.text}>Jacob's PortFolio</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Intro;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Intro from "./components/Intro";
+
 export default function Home() {
-  return;
+  return <Intro />;
 }
