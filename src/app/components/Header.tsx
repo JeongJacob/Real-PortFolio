350fd860608576cd6470b8ef9ed0f4b1bc98fb8d
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-06-15 19:49:42 +0900

    Refactor: 전역 상태 관리 변경

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,22 +1,18 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import header from "@/app/components/styles/Header.module.scss";
+import { useMenuStore } from "@/store";
 import { IoMenuOutline, IoCloseOutline } from "react-icons/io5";
 
 const Header = () => {
-  const [onMenu, setOnMenu] = useState(false);
+  const onMenu = useMenuStore((state) => state.onMenu);
+  const handleMenu = useMenuStore((state) => state.handleMenu);
   return (
     <header className={header.wrapper}>
       {!onMenu ? (
-        <IoMenuOutline
-          className={header.menu__button}
-          onClick={() => setOnMenu(true)}
-        />
+        <IoMenuOutline className={header.menu__button} onClick={handleMenu} />
       ) : (
-        <IoCloseOutline
-          className={header.menu__button}
-          onClick={() => setOnMenu(false)}
-        />
+        <IoCloseOutline className={header.menu__button} onClick={handleMenu} />
       )}
     </header>
   );
