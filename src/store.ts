2accaaea4f80ee1921bfc93e29d043397d6714b6
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-06-16 21:26:01 +0900

    Feat: Title 전역상태 추가

--- src/store.ts
+++ src/store.ts
@@ -4,8 +4,16 @@ interface MenuState {
   onMenu: boolean;
   handleMenu: () => void;
 }
+interface TitleState {
+  onTitle: string;
+  handleTitle: (title: string) => void;
+}
 
 export const useMenuStore = create<MenuState>((set) => ({
   onMenu: false,
   handleMenu: () => set((state) => ({ onMenu: !state.onMenu })),
+}));
+export const useTitleStore = create<TitleState>((set) => ({
+  onTitle: "PIKCHA",
+  handleTitle: (title: string) => set(() => ({ onTitle: title })),
 }));
