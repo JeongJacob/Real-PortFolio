9d42d545c79320a1942ed39b5a662eda2761659a
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-06-17 00:47:22 +0900

    Feat: ProjectModal 전역변수 추가

--- src/store.ts
+++ src/store.ts
@@ -9,11 +9,22 @@ interface TitleState {
   handleTitle: (title: string) => void;
 }
 
+interface ModalState {
+  onModal: boolean;
+  handleModal: () => void;
+}
+
 export const useMenuStore = create<MenuState>((set) => ({
   onMenu: false,
   handleMenu: () => set((state) => ({ onMenu: !state.onMenu })),
 }));
+
 export const useTitleStore = create<TitleState>((set) => ({
   onTitle: "PIKCHA",
   handleTitle: (title: string) => set(() => ({ onTitle: title })),
+}));
+
+export const useModalStore = create<ModalState>((set) => ({
+  onModal: false,
+  handleModal: () => set((state) => ({ onModal: !state.onModal })),
 }));
