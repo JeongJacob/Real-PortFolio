fb4a1e8dc7d662fce444edaa60c051de3545b737
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-06-16 15:27:34 +0900

    Feat: Introduce 구현

--- src/app/introduce/page.tsx
+++ src/app/introduce/page.tsx
@@ -1,26 +1,26 @@
-import React from "react";
-import ImageSlider from "@/app/components/ImageSlider";
+"use client";
+import React, { useEffect, useState } from "react";
+import ImageSlider, { SliderProps } from "@/app/components/ImageSlider";
+import axios from "axios";
 
 const Introduce = () => {
-  const sampleData = [
-    {
-      image: "/jacob1.png",
-      caption: "안녕하세요",
-    },
-    {
-      image: "/jacob2.png",
-      caption: "안녕하세요",
-    },
-    {
-      image: "/jacob4.png",
-      caption: "안녕하세요",
-    },
-  ];
-  return (
-    <div>
-      <ImageSlider SliderData={sampleData}></ImageSlider>
-    </div>
-  );
+  const [introduceData, setIntroduceData] = useState<SliderProps[]>([]);
+
+  const getIntroduceData = () => {
+    const fetchData = async () => {
+      axios
+        .get("/api/introduce")
+        .then((res) => setIntroduceData(res.data))
+        .catch((err) => console.error(err));
+    };
+    fetchData();
+  };
+
+  useEffect(() => {
+    getIntroduceData();
+  }, []);
+
+  return <ImageSlider SliderData={introduceData}></ImageSlider>;
 };
 
 export default Introduce;
