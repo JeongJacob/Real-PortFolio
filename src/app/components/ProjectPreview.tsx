126d2c89668db2066080f5cb14a1ca0527dbaf31
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-06-17 00:45:31 +0900

    Design: ProjectPreView 레이아웃 수정

--- src/app/components/ProjectPreview.tsx
+++ src/app/components/ProjectPreview.tsx
@@ -1,30 +1,39 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import { ProjectProps } from "../project/page";
+import ProjectInfoModal from "./ProjectInfoModal";
+import { useModalStore } from "@/store";
 import project from "@/app/styles/Project.module.scss";
-import Link from "next/link";
 
 const ProjectPreview = ({ projectData }: { projectData: ProjectProps }) => {
+  const { onModal, handleModal } = useModalStore();
   return (
-    <div className={project.content__container}>
-      <h2 className={project.content__title}>{projectData.title}</h2>
-      <hr />
-      <div className={project.content__image__container}>
-        <Image
-          src={projectData.image}
-          alt="홈페이지"
-          width={350}
-          height={350}
-          className={project.content__image}
-        />
-      </div>
-      <Link
-        href={`/project/${projectData.name}`}
-        className={project.content__moreinfo__btn}
-      >
-        More Info
-      </Link>
-    </div>
+    <>
+      {onModal ? (
+        <ProjectInfoModal projectData={projectData} />
+      ) : (
+        <div className={project.content__container}>
+          <h2 className={project.content__title}>{projectData.title}</h2>
+          <hr />
+          <div className={project.content__image__container}>
+            <Image
+              src={projectData.image}
+              alt="홈페이지"
+              width={350}
+              height={350}
+              className={project.content__image}
+            />
+          </div>
+          <button
+            className={project.content__moreinfo__btn}
+            onClick={handleModal}
+          >
+            More Info
+          </button>
+        </div>
+      )}
+    </>
   );
 };
 
