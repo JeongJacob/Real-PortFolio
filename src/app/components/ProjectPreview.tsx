29b48cde2e0cf02199989183d962ce499b9e7977
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-06-20 13:54:10 +0900

    Design: SNS 링크 추가 및 레이아웃 수정

--- src/app/components/ProjectPreview.tsx
+++ src/app/components/ProjectPreview.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 import { ProjectProps } from "../project/page";
 import ProjectInfoModal from "./ProjectInfoModal";
 import { useModalStore } from "@/store";
-import project from "@/app/styles/Project.module.scss";
 import Link from "next/link";
+import project from "@/app/styles/Project.module.scss";
 
 const ProjectPreview = ({ projectData }: { projectData: ProjectProps }) => {
   const { onModal, handleModal } = useModalStore();
@@ -14,34 +14,41 @@ const ProjectPreview = ({ projectData }: { projectData: ProjectProps }) => {
       {onModal ? (
         <ProjectInfoModal projectData={projectData} />
       ) : (
-        <div className={project.content__container}>
+        <div className={project.content__wrapper}>
           <h2 className={project.content__title}>{projectData.title}</h2>
           <hr />
-          <div className={project.content__image__container}>
-            <Link href={projectData.link} target="blink">
-              <Image
-                src={projectData.image}
-                alt="홈페이지"
-                width={350}
-                height={350}
-                className={project.content__image}
-              />
-            </Link>
-          </div>
-          <div className={project.content__moreinfo__btn__container}>
-            <Link
-              href={projectData.github}
-              target="blink"
-              className={project.content__moreinfo__btn}
-            >
-              Github
-            </Link>
-            <button
-              className={project.content__moreinfo__btn}
-              onClick={handleModal}
-            >
-              More Info
-            </button>
+          <div className={project.content__container}>
+            <div className={project.content__image__container}>
+              <Link href={projectData.link} target="blink">
+                <Image
+                  src={projectData.image}
+                  alt="홈페이지"
+                  width={350}
+                  height={350}
+                  className={project.content__image}
+                />
+              </Link>
+            </div>
+            <div className={project.content__introduce__info__container}>
+              <div className={project.content__introduce__container}>
+                <p>{projectData.introduce}</p>
+              </div>
+              <div className={project.content__moreinfo__btn__container}>
+                <Link
+                  href={projectData.github}
+                  target="blink"
+                  className={project.content__moreinfo__btn}
+                >
+                  Github
+                </Link>
+                <button
+                  className={project.content__moreinfo__btn}
+                  onClick={handleModal}
+                >
+                  More Info
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       )}
