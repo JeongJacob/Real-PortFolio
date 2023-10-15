dc779834e4c292b7ba9e586d597ef1225b716110
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-10-15 21:45:25 +0900

    Feat: SkillBody Interface 설정

--- src/app/components/MuiCard.tsx
+++ src/app/components/MuiCard.tsx
@@ -5,32 +5,25 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 import skill from "@/app/styles/Skill.module.scss";
+import { SkillBodyT } from "../skill/page";
 
-export default function MuiCard({
-  src,
-  title,
-  body,
-}: {
-  src: string;
-  title: string;
-  body: string;
-}) {
+export default function MuiCard(props: SkillBodyT) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
         <CardMedia
           component="img"
           height="140"
-          image={src}
+          image={props.src}
           alt="green iguana"
           className={skill.card__element}
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            {title}
+            {props.title}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            {body}
+            {props.body}
           </Typography>
         </CardContent>
       </CardActionArea>
